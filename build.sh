fa02acebac174a0ec76bd1b99a8fa742cb327419
Kaustav Ray <kkray1345@gmail.com>
2025-08-04 17:10:52 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
